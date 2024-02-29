fe78744dcd74a88d66126dbf7b466eacbd897916
AbdulrahmanM <abdodj18@gmail.com>
2024-02-29 15:44:51 +0300

    creat the video background section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         setLogo(!logo)
     }
     return (
-        <div className='flex justify-between items-center h-20 px-4'>
+        <div className='flex justify-between items-center h-20 px-4 absolute z-10 text-white w-full'>
             <div>
                 <h1  onClick={handleNav} className={logo ? 'hidden':'block'}>BEACHES.</h1>
             </div>
@@ -31,7 +31,7 @@ const Navbar = () => {
             <div onClick={handleNav} className='md:hidden z-10'>
                 {nav ? <AiOutlineClose className='text-black' size={20}/>:<HiOutlineMenuAlt4 size={20}/>}
             </div>
-            <div onClick={handleNav} className={nav?'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col':'absolute left-[-100%]'}>
+            <div onClick={handleNav} className={nav?'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col':'absolute left-[-100%]'}>
                 <ul>
                     <h1>BEACHES.</h1>
                     <li className='border-b '>Home</li>
